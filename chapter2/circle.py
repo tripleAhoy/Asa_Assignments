9790aa00af5147762fd8cb36db8fa21e41c323bc
Adewale Adegbite <adegbiteolawale44@gmail.com>
2025-05-11 04:00:46 +0100

    Add circle.py file

---
+++ chapter2/circle.py
@@ -0,0 +1,9 @@
+radius = 2
+diameter = 2 * radius
+pi = 3.14159 
+circumference = 2 * pi * radius
+area = pi * radius ** 2
+
+print('diameter is ', diameter)
+print('circumference is ', circumference)
+print('area is', area)
