b48d692cedd788b686d371afca680a2a5f5029f1
Adewale Adegbite <adegbiteolawale44@gmail.com>
2025-05-11 04:10:46 +0100

    Add question1.py file

---
+++ chapter2/question1.py
@@ -0,0 +1,17 @@
+#1
+x = 2
+y = 3
+
+print('x =', x) # x = 2
+print('Value of', x, '+', x, 'is', (x + x)) # Value of 2 + 2 is 4
+print('x =') # x =
+print((x + y), '=', (y + x)) # 5 = 5
+
+
+#2 
+the data type is not casted to int to collect the variable.
+
+#3
+if grade >= 90:
+	print('Congratulations! Your grade of 91 earns you an A in this course')
+
