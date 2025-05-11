296a03148b1c702ae179e35d26f81d79411339ae
Adewale Adegbite <adegbiteolawale44@gmail.com>
2025-05-11 04:13:36 +0100

    Add tablesquarecube.py file

---
+++ chapter2/tablesquarecube.py
@@ -0,0 +1,19 @@
+number = 0
+
+
+print('number	square	cube')
+print(number, '\t', number ** 2, '\t', number ** 3)
+print(number + 1, '\t',  (number + 1) ** 2, '\t', (number + 1) ** 3)
+print(number + 2, '\t',  (number + 2) ** 2, '\t', (number + 2) ** 3)
+print(number + 3, '\t',  (number + 3) ** 2, '\t', (number + 3) ** 3)
+print(number + 4, '\t',  (number + 4) ** 2, '\t', (number + 4) ** 3)
+print(number + 5, '\t',  (number + 5) ** 2, '\t', (number + 5) ** 3)
+
+print('number	square	cube')
+print('    ', number, '    ',number ** 2,'    ', number ** 3)
+print('    ',number + 1, '    ',  (number + 1) ** 2, '    ', (number + 1) ** 3)
+print('    ',number + 2, '    ',  (number + 2) ** 2, '    ', (number + 2) ** 3)
+print('    ',number + 3, '    ',  (number + 3) ** 2, '   ', (number + 3) ** 3)
+print('    ',number + 4, '   ',  (number + 4) ** 2, '   ', (number + 4) ** 3)
+print('    ',number + 5, '   ',  (number + 5) ** 2, '  ', (number + 5) ** 3)
+
