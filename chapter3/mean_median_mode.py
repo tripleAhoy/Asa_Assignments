5c60f713906555f7171a7482f136dc1c9e5aa9d4
Adewale Adegbite <adegbiteolawale44@gmail.com>
2025-05-18 13:21:35 +0100

    Add mean_median_mode.py

---
+++ chapter3/mean_median_mode.py
@@ -0,0 +1,10 @@
+#28
+
+import statistics
+
+values = [9, 11,17,22,22,22,34,34,34,40]
+	 
+
+print('the mean value is:', statistics.mean(values))
+print('the mode value is:', statistics.mode(values))
+print('the median value is:', statistics.median(values))
