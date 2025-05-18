5d4165960b49e59b1ea77ba5ac451f146428ae1b
Adewale Adegbite <adegbiteolawale44@gmail.com>
2025-05-18 13:08:41 +0100

    Add median.py file

---
+++ chapter3/median.py
@@ -0,0 +1,7 @@
+#29
+
+#if the two values are different the calculated mean may not correspond to the actual values in a dataset
+
+values = [9, 11,17,22,22,22,34,34,34,40,45,33]
+
+print('the median value is:', statistics.median(values))
