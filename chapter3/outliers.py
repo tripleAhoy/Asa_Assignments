02f311795a49640c4844e947bbc5c4e13ef14557
Adewale Adegbite <adegbiteolawale44@gmail.com>
2025-05-18 13:34:11 +0100

    Add outliers.py

---
+++ chapter3/outliers.py
@@ -0,0 +1,6 @@
+#30
+
+the median is the most affected due to changes in value caused by the introduction of an outlier
+
+The mode is the least affected by outliers, because it only deals with the number of occurence
+and it is rarely affected by an outlier.
